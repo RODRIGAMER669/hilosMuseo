31668fc55d9839bc40b9ff2707f5237857b10a55
RODRIGAMER669 <alexis.rr0501@gmail.com>
2025-11-13 20:46:14 +0100

    sincronizacion

--- demo/src/main/java/com/psp/hilos/museo/Main.java
+++ demo/src/main/java/com/psp/hilos/museo/Main.java
@@ -1,18 +1,20 @@
 package com.psp.hilos.museo;
 
 public class Main {
+    public final static int TOTAL = 100;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         Sala sala = new Sala();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000; i++) {
             Thread entrada = new Thread(new Entrada(sala));
             entrada.start();
         }
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 1000; i++) {
             Thread salida = new Thread(new Salida(sala));
             salida.start();
         }
+        Thread.sleep(TOTAL*10);
         System.out.println("Aforo final: " + sala.aforo);
     }
 }
